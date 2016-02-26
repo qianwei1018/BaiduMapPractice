69a81f9af7dc124fd1ae545e462be6fe3e02032d
sq-ios25 <sq-ios25@sq-ios25demacbook-air.local>
2016-02-26 09:40:11 +0800

    Initial Commit

---
+++ BaiduMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaiduMap
+//
+//  Created by sq-ios25 on 16/2/26.
+//  Copyright © 2016年 sq-ios25. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BaiduMap/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BaiduMap
+//
+//  Created by sq-ios25 on 16/2/26.
+//  Copyright © 2016年 sq-ios25. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
